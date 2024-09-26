241a11dbbf780f5e3d5ff6b1d8b318e897a4ca54
Karin Wegner <karinwegner00@gmail.com>
2024-09-26 15:59:13 +0200

    Added interface IUseable to potion

---
+++ GameProject/Items/IUseable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject.Items
+{
+    internal interface IUseable
+    {
+        void Use(Creature creature);
+        void Use(Creature creature, Action<Creature> action);
+    }
+}
